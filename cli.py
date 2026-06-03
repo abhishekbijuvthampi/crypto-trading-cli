706d2fb3f3db59bcbd9e096ee60edea6f9f31b94
abhishekbijuvthampi <abhishekbijuvthampi@gmail.com>
2026-06-03 05:35:56 +0530

    Added the skeleton of CLI for market_trades and account_info

--- cli.py
+++ cli.py
@@ -6,20 +6,49 @@ parser = argparse.ArgumentParser(
 
 subparsers = parser.add_subparsers(dest="command")
 
+
+# Subcommand Acoount
 parser_account = subparsers.add_parser(
     "account",
-    help="To Account Details"
+    help="to Account Details"
     )
 parser_account.add_argument(
     '-a', 
     '--asset', 
     type=str, 
-    help='To Check Asset (eg: BTC, USDT, BNB)'
+    help='to Check Asset (eg: BTC, USDT, BNB)'
     )
 parser_account.add_argument(
     '-l',
     '--log',
-    action='store_false'
+    help="show account logs",
+    action='store_true'
+    )
+
+
+# Subcommand Trade
+parser_trade = subparsers.add_parser(
+    "trades",
+    help="to Trade in Market"
+    )
+parser_trade.add_argument(
+    '-sb',
+    '--symbol', 
+    type=str,
+    help="symbol to place Trade",
+    required=True
+    )
+parser_trade.add_argument(
+    '-s',
+    '--side', 
+    choices=['BUY', 'SELL'],
+    help="choose : BUY or SELL (CAPS)",
+    required=True
+    )
+parser_trade.add_argument(
+    'quantity',
+    type=float,
+    help="quantity of the trade"
     )
 
 
